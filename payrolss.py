d66a9d6ee97dba643b405989e0a29e763f31c45c
sefammirza <81354116+sefammirza@users.noreply.github.com>
2021-04-12 16:15:09 +0300

    payroolss-salary

---
+++ payrolss.py
@@ -0,0 +1,7 @@
+payrolls1 = 3
+payrolls2 = 29.99
+payrolls3 = 4.1
+
+print("employee wage " + "$" + str('{:.2f}'.format(payrolls1)))
+print("employee wage " + "$" + str('{:.2f}'.format(payrolls2)))
+print("employee wage " + "$" + str('{:.2f}'.format(payrolls3)))
