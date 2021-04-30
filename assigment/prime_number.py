cda7d75e9c056d2397fd54c2eae9b837e703f693
sefammirza <81354116+sefammirza@users.noreply.github.com>
2021-04-30 23:54:11 +0300

    prime number

--- assigment/prime_number.py
+++ assigment/prime_number.py
@@ -5,11 +5,11 @@ while True:
     else:
         break
 if int(value) == 2:
-    print("This is a prime number")
+    print("This is a prime number.")
 else:
     for i in range(2, int(value)):
         if (int(value) %i) == 0:
-            print("asal sayi DEGILDIR")
+            print("This is not a prime number.")
             break
     else:
-        print("asal sayidir")
+        print("This is the prime number.")
