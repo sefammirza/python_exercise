aa9fb8522f6a23b3ed9254718f0af5af64aab304
sefammirza <81354116+sefammirza@users.noreply.github.com>
2021-04-30 23:50:17 +0300

    prime_number

---
+++ assigment/prime_number.py
@@ -0,0 +1,15 @@
+while True:
+    value = input('please entera number: ')
+    if not(value.isnumeric()):
+        print("yanlis girdin ")
+    else:
+        break
+if int(value) == 2:
+    print("bu bir asal sayidir")
+else:
+    for i in range(2, int(value)):
+        if (int(value) %i) == 0:
+            print("asal sayi DEGILDIR")
+            break
+    else:
+        print("asal sayidir")
