c73dd3b31769f7d61c59969de8c01ecbc24e7a96
sefammirza <81354116+sefammirza@users.noreply.github.com>
2021-05-01 22:39:03 +0300

    parrot

---
+++ parrot.py
@@ -0,0 +1,7 @@
+def parrot(voltage, state="a stiff", action="voom", typed="Norwegian Blue"):
+    print("-- This parrot wouldn't", action, end=" ")
+    print("if you put", voltage, "volts through it.")
+    print("-- Lovely plumage, the", typed)
+    print("-- It's", state, "!")
+    
+parrot(1000)
