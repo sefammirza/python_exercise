72273365233f5d7f72fcd7c1f4c5c4217e6c85b4
sefammirza <81354116+sefammirza@users.noreply.github.com>
2021-05-04 22:33:29 +0300

    codding chalenge

--- codding_chalenge_1.py
+++ codding_chalenge_1.py
@@ -2,4 +2,4 @@ def query(s):
     while "()" in s or "{}" in s or '[]' in s:
         s = s.replace("()", "").replace('{}', "").replace('[]', "")
     return s == ''
-print(query(('{)')))
+print(query(('{}[][]()()')))
